7398b663e49fa05ce3de4a90a82e4dc68ad3b0b1
Marina Kovaleva <marinakovaleva259@gmail.com>
2023-05-14 20:33:19 +0300

    Create navLink component

---
+++ src/components/NavLink/NavLink.styled.tsx
@@ -0,0 +1,5 @@
+import styled from '@emotion/styled';
+
+export const StyledLink = styled.a`
+    color: black;
+`
---
+++ src/components/NavLink/NavLink.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react';
+import Link from 'next/link';
+import { StyledLink } from "./NavLink.styled";
+
+const NavLink: FC<{href:string, name:string}> = ({href, name}) => {
+    return (
+        <Link href={href} passHref legacyBehavior>
+            <StyledLink>{name}</StyledLink>
+        </Link>
+    )
+}
+
+export default NavLink;
