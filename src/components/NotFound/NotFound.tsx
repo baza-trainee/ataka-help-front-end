7add7e24f4a5bd01942c0ea0dab2111007f298f9
Marina Kovaleva <marinakovaleva259@gmail.com>
2023-05-26 19:28:38 +0300

    Add container for error page

--- src/components/NotFound/NotFound.tsx
+++ src/components/NotFound/NotFound.tsx
@@ -2,6 +2,7 @@ import { FC } from "react";
 import Image from "next/image";
 
 import image from "../../assets/rectangle.png";
+import { Container } from "../Common";
 import {
   Section,
   TitleH1,
@@ -16,20 +17,22 @@ import {
 const NotFound: FC = () => {
   return (
     <Section>
-      <StyledDiv>
-        <StyledDivWrap>
-          <TitleH1>404</TitleH1>
-          <TitleH2>Ця сторінка не знайдена</TitleH2>
-          <StyledText>
-            Ми не змогли знайти сторінку, яку Ви шукаєте, або, можливо, сталася
-            помилка
-          </StyledText>
-        </StyledDivWrap>
-        <StyledDivImg>
-          <Image src={image} alt="Image" width={466} height={466} />
-        </StyledDivImg>
-      </StyledDiv>
-      <StyledLink href="/">На головну</StyledLink>
+      <Container>
+        <StyledDiv>
+          <StyledDivWrap>
+            <TitleH1>404</TitleH1>
+            <TitleH2>Ця сторінка не знайдена</TitleH2>
+            <StyledText>
+              Ми не змогли знайти сторінку, яку Ви шукаєте, або, можливо,
+              сталася помилка
+            </StyledText>
+          </StyledDivWrap>
+          <StyledDivImg>
+            <Image src={image} alt="Image" width={466} height={466} />
+          </StyledDivImg>
+        </StyledDiv>
+        <StyledLink href="/">На головну</StyledLink>
+      </Container>
     </Section>
   );
 };
