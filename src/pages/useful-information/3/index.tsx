1199a16748971be09648308507bcde42fb9323ae
kpripper <102256496+kpripper@users.noreply.github.com>
2023-05-31 22:07:53 +0300

    add texts to props

--- src/pages/useful-information/3/index.tsx
+++ src/pages/useful-information/3/index.tsx
@@ -5,11 +5,33 @@ import UsefulInformation from "@/components/UsefulInformation";
 import Slider from "@/components/Slider";
 import Header from "@/components/Header";
 
+const text1 = `Lorem ipsum dolor sit amet consectetur. Vel pellentesque lacus
+vestibulum sed aliquet orci tristique convallis vulputate.
+Imperdiet non rhoncus cursus mattis nec et vestibulum odio. Erat
+hendrerit vitae auctor diam cursus adipiscing donec pharetra.
+Imperdiet non tincidunt eu massa lacus vitae rutrum eu sodales.
+Phasellus placerat donec sed praesent. Imperdiet orci pellentesque
+duis lorem egestas phasellus. Commodo nunc eu non amet imperdiet
+nec volutpat massa.`;
+
+const text2 = `Lorem ipsum dolor sit amet consectetur. Vel pellentesque lacus
+vestibulum sed aliquet orci tristique convallis vulputate.
+Imperdiet non rhoncus cursus mattis nec et vestibulum odio. Erat
+hendrerit vitae auctor diam cursus adipiscing donec pharetra.
+Imperdiet non tincidunt eu massa lacus vitae rutrum eu sodales.
+Phasellus placerat donec sed praesent. Imperdiet orci pellentesque
+duis lorem egestas phasellus. Commodo nunc eu non amet imperdiet
+nec volutpat massa.`;
+
 const TwoFactorAuthentication: NextPage = () => {
   return (
     <UserLayout title="TwoFactorAuthentication">
-       <Slider />
-      <UsefulInformation title="Що таке двохфакторна аутентифікація"/>
+      <Slider />
+      <UsefulInformation
+        title="Що таке двохфакторна аутентифікація"
+        text1={text1}
+        text2={text2}
+      />
     </UserLayout>
   );
 };
