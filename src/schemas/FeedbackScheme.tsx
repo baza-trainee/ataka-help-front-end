e6a5ef8817b377a0e24a4cccd808ac009b9f320a
Iryna Kolesnyk <i.kolesnyk.goit@gmail.com>
2023-06-03 01:12:09 +0300

    Update FeedbackScheme.tsx

--- src/schemas/FeedbackScheme.tsx
+++ src/schemas/FeedbackScheme.tsx
@@ -4,9 +4,8 @@ import { EMAIL_REGEX, EMAIL_REGEX_RU, NAME_REGEX } from "@/constants/regex";
 export const FeedbackSchema = yup.object().shape({
   name: yup
     .string()
-    .required("Введіть ім’я")
     .matches(NAME_REGEX, "Ім'я не валідне")
-    .trim()
+    .required("Введіть ім’я")
     .min(2, "Ім’я повинно бути не менше 2 знаків")
     .max(50, "Ім’я повинно бути не більше 50 знаків"),
 
@@ -18,8 +17,7 @@ export const FeedbackSchema = yup.object().shape({
     .matches(EMAIL_REGEX_RU, "Email з доменом .ru не підтримується"),
   comment: yup
     .string()
-    .matches(NAME_REGEX, "Ім'я не валідне")
-    .trim()
+    .matches(NAME_REGEX, "Ім'я не валідне")  
     .required("Введіть повідомлення")
     .max(300, "Просимо скоротити ваше повідомлення до 300 знаків"),
 });
