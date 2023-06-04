62526cd99a0f2c4498a96d28f4bf2eb4b90d3275
Iryna Kolesnyk <i.kolesnyk.goit@gmail.com>
2023-06-04 23:27:31 +0300

    fix axios interseptor

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,29 +1,50 @@
-import axios, { Axios } from 'axios';
-import { toast } from 'react-toastify';
+import axios, { Axios } from "axios";
+import { toast } from "react-toastify";
 
-const BASE_URL = "https://foradmin.fun"
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL,
+  isServer = typeof window === "undefined";
 
 export const axiosPublic = axios.create({
-    baseURL: BASE_URL,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  }) as Axios;
+  baseURL: BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+}) as Axios;
 
-  export const axiosPrivateJson = axios.create({
-    baseURL: BASE_URL,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  }) as Axios;
+export const axiosPrivateJson = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+}) as Axios;
 
-  export const axiosPrivateFormData = axios.create({
-    baseURL: BASE_URL,
-    headers: {
-      'Content-Type': 'multipart/form-data',
-    },
-  }) as Axios;
+export const axiosPrivateFormData = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    "Content-Type": "multipart/form-data",
+  },
+}) as Axios;
 
+// axiosPrivateJson.interceptors.request.use(async config => {
+//   if (isServer) {
+//     const { cookies } = await import("next/headers"),
+//       token = cookies().get("token")?.value;
+
+//     if (token) {
+//       config.headers["Authorization"] = `Bearer ${token}`;
+//     }
+//   } else {
+//     const token = document.cookie.replace(
+//       /(?:(?:^|.*;\s*)token\s*=\s*([^;]*).*$)|^.*$/,
+//       "$1",
+//     );
+
+//     if (token) {
+//       config.headers["Authorization"] = `Bearer ${token}`;
+//     }
+//   }
+//   return config;
+// });
 
 //   axiosPrivate.interceptors.request.use(
 //     async config => {
@@ -52,7 +73,7 @@ export const axiosPublic = axios.create({
 //       return Promise.reject(error);
 //     }
 //   );
-  
+
 //   axiosPublic.interceptors.response.use(
 //     async response => {
 //       if ((response.data.status = 201 && response.config.url === '/login')) {
@@ -67,7 +88,7 @@ export const axiosPublic = axios.create({
 //       }
 //       return response;
 //     },
-//     async error => {  
+//     async error => {
 //       if (!error.response) {
 //         toast.error('Please check your internet connection and try again!');
 //       }
@@ -89,7 +110,7 @@ export const axiosPublic = axios.create({
 //       ) {
 //         toast.error('User with such username is already exist.');
 //       }
-//       if (error.response.status === 403 && error.config.url === '/auth/refresh') {     
+//       if (error.response.status === 403 && error.config.url === '/auth/refresh') {
 //         toast.error('Please sign in!');
 //         return (window.location.href = '/auth');
 //       }
@@ -99,10 +120,10 @@ export const axiosPublic = axios.create({
 //       return Promise.reject(error);
 //     }
 //   );
-  
+
 //   axiosPrivate.interceptors.response.use(
 //     response => response,
-//     async error => { 
+//     async error => {
 //       if (!error.response) {
 //         toast.error('Please check your internet connection and try again!');
 //       }
@@ -110,7 +131,7 @@ export const axiosPublic = axios.create({
 //         toast.error('Please sign in!');
 //         return (window.location.href = '/auth');
 //       }
-//       if (error.response.status === 403 && error.config.url === '/auth/logout') {      
+//       if (error.response.status === 403 && error.config.url === '/auth/logout') {
 //         toast.error('Please sign in!');
 //         return (window.location.href = '/auth');
 //       }
