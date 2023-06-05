9e2f5ce8de1f13792c1a3fa344c80d039578b885
Iryna Kolesnyk <i.kolesnyk.goit@gmail.com>
2023-06-05 15:12:59 +0300

    fix bugs

--- src/constants/regex.ts
+++ src/constants/regex.ts
@@ -6,3 +6,5 @@ export const EMAIL_REGEX_RU = /^[a-zA-Z0-9._]+@[a-zA-Z0-9.-]+(?<!\.ru)$/;
 export const EMAIL_REGEX = /^[a-zA-Z0-9._]+@[a-zA-Z0-9-]+\.[a-zA-Z]{2,}$/;
 
 export const NAME_REGEX = /^(?!\s+$).*$/
+
+export const COMMENT_REGEX = /^(?!\s+$)[\s\S]*$/
