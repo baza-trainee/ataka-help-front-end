ec36f925b18f7c078505764b973c55c7681931b7
I-Kolesnyk <106455256+I-Kolesnyk@users.noreply.github.com>
2023-06-19 00:42:40 +0300

    adds dynamic loading

--- src/pages/contacts/index.tsx
+++ src/pages/contacts/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 
 import UserLayout from "@/components/UserLayout/UserLayout";
 import FeedbackForm from "@/components/FeedbackForm";
+import FeedbackClient from "@/components/FeedbackForm/FeedbackClient";
 import ContactsSection from "@/components/Contacts";
 
 import { ContactsPagePropsType } from "@/types/PagesTypes";
@@ -16,7 +17,7 @@ const Contacts: NextPage<ContactsPagePropsType> = ({ contacts, report }) => {
   return (
     <UserLayout title="Contacts" contacts={contacts} report={report}>
       <ContactsSection contacts={contacts} />
-      <FeedbackForm />
+      <FeedbackClient />
     </UserLayout>
   );
 };
