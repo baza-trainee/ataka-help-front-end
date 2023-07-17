4ba0255f7c9f6d1ea7a4cb54d22c5fa64e046969
Lysak-Anna <hvlysak@gmail.com>
2023-07-17 14:02:39 +0300

    add payment modal

--- src/components/CardModal/CardModal.tsx
+++ src/components/CardModal/CardModal.tsx
@@ -1,5 +1,6 @@
 import { FC } from "react";
 import { ItemTextStyled, ListTextStyled, TextStyled } from "./CardModal.styled";
+import { Container } from "../Common";
 
 interface CardModalProps {
   description: string[];
@@ -7,14 +8,16 @@ interface CardModalProps {
 
 const CardModal: FC<CardModalProps> = ({ description }) => {
   return (
-    <ListTextStyled>
-      {description.map((text, idx) => (
-        <ItemTextStyled key={idx}>
-          {idx + 1}
-          <TextStyled>{text}</TextStyled>
-        </ItemTextStyled>
-      ))}
-    </ListTextStyled>
+    <Container>
+      <ListTextStyled>
+        {description.map((text, idx) => (
+          <ItemTextStyled key={idx}>
+            {idx + 1}
+            <TextStyled>{text}</TextStyled>
+          </ItemTextStyled>
+        ))}
+      </ListTextStyled>
+    </Container>
   );
 };
 
